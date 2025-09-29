3dc728091dad13368bbdc74aaa20c48862e9c7cc
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2025-09-29 20:41:11 +0530

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>You did it!</h1>
+  <span is-="badge">badge</span>
+</template>
+
+<style>
+@layer base, utils, components;
+
+@import '@webtui/css/base.css';
+
+
+/* Utils */
+@import '@webtui/css/utils/box.css';
+
+/* Components */
+@import '@webtui/css/components/button.css';
+@import '@webtui/css/components/typography.css';
+@import '@webtui/css/components/badge.css';
+
+/* Import theme AFTER all other WebTUI CSS */
+@import '@webtui/theme-catppuccin';
+
+
+</style>
