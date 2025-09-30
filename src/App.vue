6f3e50d92eaa1521f6c360e3636f92158a19e9b4
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2025-09-30 23:14:28 +0530

    add nav bar and buttons

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,23 @@
 
 <template>
   <div name="main">
-    <h2># Chakradhar Gorthi</h2>
     <h1>You did it!</h1>
     <span is-="badge">badge</span>
+
+
+    <!-- Shear off the top edge's padding -->
+    <div box-="square" shear-="top">
+      <div class="header">
+          <span>Left</span>
+      </div><header>
+      <h2># Chakradhar Gorthi</h2>
+      <nav id="nav-links">
+        <button variant-="maroon">Sick</button>
+        <button variant-="rosewater">Button</button>
+        <button variant-="sky">Variants</button>
+      </nav></header>
+    </div>
+
   </div>
 </template>
 
@@ -18,7 +32,23 @@ div[name="main"] {
   width: 100%;
 }
 
+
+span {
+  background-color: var(--background0);
+  padding: 0 1ch;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+}
+
+nav#nav-links {
+  display: flex;
+  gap: 1ch;
+  margin-top: 1ch;
+}
 h2 {
-  font-size: 4lh;
+  box-sizing: content-box;
 }
 </style>
